2734928621a184498f7eacaa2bc57a8a374abcf8
Jonathan Trujillo <jonathantruest@gmail.com>
2019-05-20 20:36:29 +0100

    Tests done.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,42 +31,111 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+        it('URLs are defined', function() {
+            for(feed of allFeeds){
+                expect(feed.url).toBeDefined();
+                expect(feed.url.length).not.toBe(0);
+            }
+        });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('names are defined', function() {
+            for(feed of allFeeds){
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).not.toBe(0);
+            }
+        });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('The menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+         it('is hidden by default', function() {
+            let body = document.getElementsByTagName('body').item(0);
+            expect(body).toBeDefined();
+            expect(body.classList.length).toBe(1);
+            expect(body.classList[0]).toBe("menu-hidden");
+         });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+        it('change visibility when clicked', function() {
+            //Get the menu icon and the body element
+            let menuIcon = $('.menu-icon-link');
+            let body = document.getElementsByTagName('body').item(0);
 
+            //Check that the elements exist
+            expect(menuIcon).toBeDefined();
+            expect(body).toBeDefined();
+
+            //Click the button and check that the menu is visible
+            $(menuIcon).trigger('click');
+            expect(body.classList.length).toBe(0);
+
+            //Click again the button and check that the menu is have been hide
+            $(menuIcon).trigger('click');
+            expect(body.classList.length).toBe(1);
+            expect(body.classList[0]).toBe("menu-hidden");
+        });
+    });
+    
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        beforeEach(function(done){
+            loadFeed(0, function(){
+                done();
+            });
+        });
+
+        it('are entries', function(done) {
+            let feed = $('.feed');
+            expect(feed).toBeDefined();
+            expect($(feed).children().length).toBeGreaterThan(0);
+            done();
+        });
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
-
+    describe('New Feed Selection', function() {
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        let feedContent;
+        let newFeedContent;
+
+        beforeEach(function(done){
+            loadFeed(0, function(){
+                feedContent = $('.feed').text();
+                loadFeed(1, function(){
+                    newFeedContent = $('.feed').text();
+                    done();
+                });
+            });
+        });
+
+        it('content changes', function(done) {
+            expect(feedContent).toBeDefined();
+            expect(newFeedContent).toBeDefined();
+            expect(feedContent).not.toEqual(newFeedContent);
+            done();
+        });
+    });
 }());
